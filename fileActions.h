e73695d551a0c5f98d4e257049bc23863e07eb00
gcaraciolo <gcaraciolo@gmail.com>
2014-09-04 13:26:18 -0300

    rm old files

--- fileActions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILE_ACTIONS_H
-#define FILE_ACTIONS_H
-
-int openFile (const char *);
-int closeFile ();
-int verifyExtension(const char *);
-
-#endif
--- scanner.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCANNER_H_
-#define SCANNER_H_
-
-#include "globals.h"
-
-__TOKEN _SCAN();
-void readFile();
-
-#endif
