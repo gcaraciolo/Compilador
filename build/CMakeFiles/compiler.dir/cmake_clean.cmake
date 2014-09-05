6d39a3345070eae0cce1049bfcc588c04244a162
gcaraciolo <gcaraciolo@gmail.com>
2014-09-05 10:20:42 -0300

    merge branch organize project and testscanner

---
+++ build/CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/src/scanner.c.o"
+  "CMakeFiles/compiler.dir/src/compiler.c.o"
+  "CMakeFiles/compiler.dir/src/fileActions.c.o"
+  "compiler.pdb"
+  "compiler"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
